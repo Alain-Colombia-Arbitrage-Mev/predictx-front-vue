852809f76594e75971067ab790c76ed49147230c
CFW HeadDev <31333238+headdev@users.noreply.github.com>
2024-09-01 17:57:41 -0500

    update1
    update1

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
   <div
     class="w-[319px] flex flex-col items-start justify-start leading-[normal] tracking-[normal]"
   >
-    <Secciones />
+    <Secciones @section-click="handleSectionClick" />
     <section
       class="h-[220px] border-light-border border-t-[1px] border-solid box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-[29px] px-14"
     >
@@ -14,20 +14,29 @@
           loading="lazy"
           alt=""
           src="/rectangle1@2x.png"
-        /><Button1 />
+        />
+        <Button1 />
       </div>
     </section>
     <AccountSettings />
+    <router-view />
   </div>
 </template>
-<script>
-  import { defineComponent } from "vue";
-  import Secciones from "./Secciones.vue";
-  import Button1 from "./Button1.vue";
-  import AccountSettings from "./AccountSettings.vue";
 
-  export default defineComponent({
-    name: "Sidebar",
-    components: { Secciones, Button1, AccountSettings },
-  });
+<script>
+import { defineComponent } from "vue";
+import Secciones from "./Secciones.vue";
+import Button1 from "./Button1.vue";
+import AccountSettings from "./AccountSettings.vue";
+
+export default defineComponent({
+  name: "Sidebar",
+  components: { Secciones, Button1, AccountSettings },
+  emits: ['section-change'],
+  methods: {
+    handleSectionClick(section) {
+      this.$emit('section-change', section);
+    }
+  }
+});
 </script>
