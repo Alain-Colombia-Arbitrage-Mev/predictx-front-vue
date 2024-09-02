7bfbc5785196a8bc335246788e9e21e48b7fb8b1
CFW HeadDev <31333238+headdev@users.noreply.github.com>
2024-09-01 21:05:04 -0500

    actualizando traductor
    actualizando traductor

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,6 +3,12 @@
     class="h-screen w-[319px] flex flex-col items-start justify-start leading-[normal] tracking-[normal]"
   >
     <Secciones @section-click="handleSectionClick" />
+    <div class="self-stretch flex flex-row items-center justify-start py-2.5 pl-0 pr-[78px] gap-px">
+      <select v-model="$i18n.locale" class="bg-gray-700 text-white rounded-md px-3 py-2 w-full max-w-xs">
+        <option value="es">Español</option>
+        <option value="en">Inglés</option>
+      </select>
+    </div>
     <section
       class="h-[220px] border-light-border border-t-[1px] border-solid box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-[29px] px-14"
     >
